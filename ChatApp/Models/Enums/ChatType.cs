0b242a39f1a6fe284055fc28c57d9d18a1248cc6
kuzana <kuzana@leavingstone.com>
2021-05-11 20:47:22 +0400

    Refactor: ChatType and UserRole

--- ChatApp/Models/Enums/ChatType.cs
+++ ChatApp/Models/Enums/ChatType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ChatApp.Models.Enums
+﻿namespace ChatApp.Models.Enums
 {
     public enum ChatType
     {
